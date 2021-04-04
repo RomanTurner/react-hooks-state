91e49ba9d5c7ca70adc6886ae9e4cf8a9b848afb
Roman Turner <rn.turner@live.com>
2021-04-04 13:44:45 -0700

    assignment complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,14 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(count, "pre count");
     setCount(count + 1);
+    console.log(count, "count");
+    setCount((currentCount) => {
+      console.log(currentCount, "current count");
+      return currentCount + 1;
+    });
+    console.log(count, "post count");
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
